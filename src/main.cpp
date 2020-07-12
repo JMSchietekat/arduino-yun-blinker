d573a77aa073663c158cb58588674855fabaf8a9
JMSchietekat <justinschietekat@gmail.com>
2020-07-12 12:41:56 +0200

    Create and move timer code to external file.

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,7 @@
 #include <Arduino.h>
+#include "time_slicer.h"
 
 char input = 0;
-
-void initTimer1()
-{
-	noInterrupts(); // disable all interrupts
-	TCCR1A = 0;		// Timer/Counter1 Control Register A
-	TCCR1B = 0;		// Timer/Counter1 Control Register B
-	TCNT1 = 0;		// Timer/Counter1
-
-	OCR1A = 31250;			 // compare match register 16MHz/256/2Hz
-	TCCR1B |= (1 << WGM12);	 // CTC mode
-	TCCR1B |= (1 << CS12);	 // 256 prescaler
-	TIMSK1 |= (1 << OCIE1A); // enable timer compare interrupt
-
-	interrupts(); // enable all interrupts
-}
 
 ISR(TIMER1_COMPA_vect) // timer compare interrupt service routine
 {

15b5b8b882da6ccd9bd05a4c22d79cdd6611bd4c
JMSchietekat <justinschietekat@gmail.com>
2020-07-12 12:27:59 +0200

    Setup timer 1 with interrupt to toggle led.

--- src/main.cpp
+++ src/main.cpp
@@ -2,24 +2,41 @@
 
 char input = 0;
 
+void initTimer1()
+{
+	noInterrupts(); // disable all interrupts
+	TCCR1A = 0;		// Timer/Counter1 Control Register A
+	TCCR1B = 0;		// Timer/Counter1 Control Register B
+	TCNT1 = 0;		// Timer/Counter1
+
+	OCR1A = 31250;			 // compare match register 16MHz/256/2Hz
+	TCCR1B |= (1 << WGM12);	 // CTC mode
+	TCCR1B |= (1 << CS12);	 // 256 prescaler
+	TIMSK1 |= (1 << OCIE1A); // enable timer compare interrupt
+
+	interrupts(); // enable all interrupts
+}
+
+ISR(TIMER1_COMPA_vect) // timer compare interrupt service routine
+{
+	digitalWrite(LED_BUILTIN, digitalRead(LED_BUILTIN) ^ 1); // toggle LED pin
+}
+
 void setup()
 {
-  pinMode(LED_BUILTIN, OUTPUT);
+	pinMode(LED_BUILTIN, OUTPUT);
 
-  Serial.begin(9600);
+	Serial.begin(9600);
+	initTimer1();
 }
 
 void loop()
 {
-  digitalWrite(LED_BUILTIN, HIGH);
-  delay(50);
-  digitalWrite(LED_BUILTIN, LOW);
-  delay(50);
 
-  while (Serial.available() > 0)
-  {
-    input = Serial.read() +1;
+	while (Serial.available() > 0)
+	{
+		input = Serial.read() + 1;
 
-    Serial.print(input);
-  }
+		Serial.print(input);
+	}
 }

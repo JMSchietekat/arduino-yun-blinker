861e0b8792df16dfa27ef3a07d823aa406ef7d87
JMSchietekat <justinschietekat@gmail.com>
2020-07-12 12:10:04 +0200

    Receive char and return char +1.

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,25 @@
 #include <Arduino.h>
 
+char input = 0;
+
 void setup()
 {
   pinMode(LED_BUILTIN, OUTPUT);
+
+  Serial.begin(9600);
 }
 
 void loop()
 {
   digitalWrite(LED_BUILTIN, HIGH);
-  delay(1000);
+  delay(50);
   digitalWrite(LED_BUILTIN, LOW);
-  delay(1000);
+  delay(50);
+
+  while (Serial.available() > 0)
+  {
+    input = Serial.read() +1;
+
+    Serial.print(input);
+  }
 }
